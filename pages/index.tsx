7a9316bdb103c2f5b143d6ee5d600beff0461972
fdemir <frndemir@gmail.com>
2022-08-16 02:33:16 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div
+      style={{
+        padding: "25%",
+      }}
+    >
+      <h1>Etymology!</h1>
+    </div>
+  );
+};
+
+export default Home;
